08de4f33797f68e1ab10b099010899860ae64ec0
Bryan <hanson@depauw.edu>
2021-11-06 08:09:19 -0700

    save status after local build

--- inst/tinytest/test-PCAtoXhat.R
+++ inst/tinytest/test-PCAtoXhat.R
@@ -1,6 +1,6 @@
 # File created by roxut; edit the function definition file, not this file
 
-# Test found in PCAtoXhat.R:20 (file:line)
+# Test found in PCAtoXhat.R:22 (file:line)
   
 # Data from ?prcomp
 C <- chol(S <- toeplitz(.9 ^ (0:31)))
@@ -8,10 +8,13 @@ set.seed(17)
 X <- matrix(rnorm(32000), 1000, 32)
 Z <- X %*% C
 pca <- prcomp(Z)
+prin <- princomp(Z)
 
 # Test that original data set is returned when ncomp = ncol(pca$x)
 tst1 <- PCAtoXhat(pca)
 expect_true(all.equal(tst1, Z, check.attributes = FALSE))
+tst2 <- PCAtoXhat(prin)
+expect_true(all.equal(tst2, Z, check.attributes = FALSE))
 
 # Test for handling of bad arguments
 expect_error(PCAtoXhat(pca, 35))
